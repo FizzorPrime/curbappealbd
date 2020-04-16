24ac03ed3049b47ce6a3a3e0788f0cdcb9e5765a
FizzorPrime <62304180+FizzorPrime@users.noreply.github.com>
2020-04-16 18:05:18 -0500

    Delete PublisherRepository.java

--- curbappeal/src/main/java/com/curbappealbd/curbappeal/repositories/PublisherRepository.java
+++
@@ -1,7 +0,0 @@
-package com.curbappealbd.curbappeal.repositories;
-
-import com.curbappealbd.curbappeal.domain.Publisher;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PublisherRepository extends CrudRepository<Publisher, Long> {
-}
