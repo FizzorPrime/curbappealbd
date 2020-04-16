86123267a8d0b691ad7956cb63022d4e5420b82c
FizzorPrime <62304180+FizzorPrime@users.noreply.github.com>
2020-04-16 18:05:05 -0500

    Delete AuthorRepository.java

--- curbappeal/src/main/java/com/curbappealbd/curbappeal/repositories/AuthorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.curbappealbd.curbappeal.repositories;
-
-import com.curbappealbd.curbappeal.domain.Author;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AuthorRepository extends CrudRepository<Author, Long> {
-
-}
