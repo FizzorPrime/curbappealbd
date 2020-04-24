f1c6e68fefebe0197bcfbee19a94cd55f8903c80
FizzorPrime <62304180+FizzorPrime@users.noreply.github.com>
2020-04-24 14:30:22 -0500

    Add files via upload

--- curbappeal/src/main/webapp/reactjs/src/App.js
+++ curbappeal/src/main/webapp/reactjs/src/App.js
@@ -1,51 +1,80 @@
-import React, {useEffect, useState} from 'react';
-import png from './img1.png';
+import React, {useEffect, useReducer, useState} from 'react';
 import './App.css';
-
-const fileUploadHandler = (file) => {
-    if (file !== null) {
-        console.log(file);
-    }
-}
+import axios from 'axios';
+import ImageCard from "./ImageCard";
 
 function App() {
 
-    const [selectedFile, setSelectedFile] = useState(null);
-    const [uploadText, setUploadText] = useState("Upload file here");
+    const [allImages, setAllImages] = useState([]);
+    const [_count, forceUpdate] = useReducer(x => x + 1, 0);
 
-    //useEffect()
+    const myWidget = window.cloudinary.createUploadWidget({
+            cloudName: 'fizzor',
+            uploadPreset: 'default-unsigned'
+        }, (error, result) => {
+            if (!error && result && result.event === "success") {
+                console.log('Done! Here is the image info: ', result.info);
+                postImage(result.info);
+            }
+        }
+    );
+
+    function postImage(file) {
+        let body = {
+            name : file.original_filename,
+            type : file.format,
+            url : file.url
+        };
+        let currentImages = allImages;
+        console.log(JSON.stringify(body));
+        axios.post('http://localhost:8080/images', JSON.stringify(body),{
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+            .then(response => {
+                currentImages.push(response.data);
+                currentImages = currentImages.sort((a,b) => a.position - b.position);
+                setAllImages(currentImages);
+            }).then(() => {
+                myWidget.close();
+        })
+            .catch(err => console.error(err));
+        forceUpdate();
+    }
+
+    function cloudinaryUpload(e) {
+        e.preventDefault();
+        myWidget.open();
+    }
+
+    useEffect(() => {
+        axios.get('http://localhost:8080/images')
+            .then(res => {
+                let sortedImages = res.data.sort((a,b) => a.position - b.position);
+                setAllImages(sortedImages);
+                }
+            );
+
+    }, [_count]);
 
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={png} className="App-logo" alt="logo" style={{zIndex:0}}/>
-                <p style={{zIndex:1}}>
-                    Hello World
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-                <input type="file" onChange={event => {
-                    //console.log(event.target.files[0].type);
-                    if (event.target.files[0].type === "image/png" || event.target.files[0].type === "image/jpeg") {
-                        setSelectedFile(event.target.files[0]);
-                        setUploadText("Correct file type");
-                    } else {
-                        setSelectedFile(null);
-                        setUploadText("Wrong file type");
-                    }
-                }}
-                />
-                <button onClick={() => fileUploadHandler(selectedFile)}>Upload</button>
-                <p>
-                    {uploadText}
-                </p>
-            </header>
+
+            <button id="upload_widget" className="cloudinary-button" onClick={(e) => {cloudinaryUpload(e)}}>Upload Image</button>
+            <div className="gallery">
+                {
+                    !allImages.length
+                    ?
+                        <p>Loading...</p>
+                    :
+                        <>
+                            {
+                                allImages.map((image,index) => <ImageCard key={image.id} image={image} allImages={allImages} forceUpdate={forceUpdate} index={index} />)
+                            }
+                        </>
+                }
+            </div>
         </div>
     );
 }
