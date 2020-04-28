a0128f0ba5cbfc8696b0555a013a13a177d16d95
FizzorPrime <62304180+FizzorPrime@users.noreply.github.com>
2020-04-28 14:07:22 -0500

    Add files via upload

--- curbappeal/src/main/java/com/curbappealbd/curbappeal/repositories/ImageRepository.java
+++ curbappeal/src/main/java/com/curbappealbd/curbappeal/repositories/ImageRepository.java
@@ -3,6 +3,10 @@ package com.curbappealbd.curbappeal.repositories;
 import com.curbappealbd.curbappeal.domain.ImageModel;
 import org.springframework.data.repository.CrudRepository;
 
+/**
+ * ImageRepository extends the CrudRepository class to allow the ImageModel to be stored in the database
+ * @author Fizzor
+ */
 public interface ImageRepository extends CrudRepository<ImageModel, Long>{
 
 }
--- curbappeal/src/main/java/com/curbappealbd/curbappeal/restController/ImageRestController.java
+++ curbappeal/src/main/java/com/curbappealbd/curbappeal/restController/ImageRestController.java
@@ -19,6 +19,11 @@ import java.util.Base64;
 import java.util.Map;
 import java.util.Optional;
 
+/**
+ * ImageRestController is the controller that builds the database and uploads the base images for the database to store
+ * It also deals with all HTTP calls from the front end
+ * @author Fizzor
+ */
 @RestController
 public class ImageRestController implements CommandLineRunner {
 
@@ -33,13 +38,22 @@ public class ImageRestController implements CommandLineRunner {
             "api_secret", "VuE6f0qPJ_NwJMlfXSjmHLZVsXg",
             "folder", "curbappealbd"));
 
-    // Create
+    /**
+	 * Create
+	 * @author Fizzor
+	 * @param image ImageModel is the result of the json http message
+	 * 
+	 * @return returns the value of the new image after uploading it to the database
+	 */
     @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping(value = "/images", method = RequestMethod.POST)
     public ResponseEntity<Object> createImage(@RequestBody ImageModel image) {
         image.setPosition((int) imageRepository.count() + 1);
         Iterable<ImageModel> allImages = imageRepository.findAll();
         //ArrayList<Integer> currentPositions = new ArrayList<Integer>();
+        
+        // tests if the position set at the beginning is the last value or not and sets it to the highest value in the databse
+        // this prevents the image to appear randomly in the gallery when uploaded
         for(ImageModel databaseImage : allImages){
             //currentPositions.add(databaseImage.getPosition());
             if (image.getPosition() <= databaseImage.getPosition()){
@@ -60,6 +74,14 @@ public class ImageRestController implements CommandLineRunner {
         return new ResponseEntity<>(image, HttpStatus.CREATED);
     }
 
+    /**
+	 * existsIn checks for a specific position within a given ArrayList
+	 * @author Fizzor
+	 * @param list ArrayList is the list of all image positions in the database
+	 * @param toCheckValue int is the position that the algorithm is checking for
+	 * 
+	 * @return whether or not the position exists in the list or not
+	 */
     public boolean existsIn(ArrayList<Integer> list, int toCheckValue){
         for (int element : list) {
             if (element == toCheckValue) {
@@ -69,20 +91,39 @@ public class ImageRestController implements CommandLineRunner {
         return false;
     }
 
-    // Read
+    /**
+	 * Read
+	 * @author Fizzor
+	 * 
+	 * @return Gives the value of every image within the database
+	 */    
     @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping(value = "/images")
     public ResponseEntity<Object> getImage() {
         return new ResponseEntity<>(imageRepository.findAll(), HttpStatus.OK);
     }
 
+    /**
+	 * Read one image
+	 * @author Fizzor
+	 * @param id Long is the id the user is attempting to get
+	 * 
+	 * @return Gives the value of the image with the value of id
+	 */   
     @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping(value = "/images/{id}")
     public ResponseEntity<Object> getImageById(@PathVariable("id") Long id) {
         return new ResponseEntity<>(imageRepository.findById(id), HttpStatus.OK);
     }
 
-    // Update
+    /**
+	 * Update
+	 * @author Fizzor
+	 * @param id Long is the id of the image to be updated
+	 * @param image ImageModel is the new values of the image to be updated to
+	 * 
+	 * @return sends back a message saying the database was able to process the given information
+	 */   
     @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping(value = "/images/{id}", method = RequestMethod.PUT)
     public ResponseEntity<Object> updateImage(@PathVariable("id") Long id, @RequestBody ImageModel image) {
@@ -92,7 +133,13 @@ public class ImageRestController implements CommandLineRunner {
         return new ResponseEntity<>("Image is updated successsfully", HttpStatus.OK);
     }
 
-    // Delete
+    /**
+	 * Delete
+	 * @author Fizzor
+	 * @param id Long is the id of the image to be deleted
+	 * 
+	 * @return sends back a message saying the database was able to process the given command
+	 */ 
     @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping(value = "/images/{id}", method = RequestMethod.DELETE)
     public ResponseEntity<Object> delete(@PathVariable("id") Long id) throws IOException {
@@ -104,6 +151,7 @@ public class ImageRestController implements CommandLineRunner {
         return new ResponseEntity<>("Image is deleted successsfully", HttpStatus.OK);
     }
 
+    // the function that is called to build the database using the mock images stored in the program
     @Override
     public void run(String... args) throws Exception {
         // These declarations are for attempting to store the images directly to the database
@@ -141,14 +189,17 @@ public class ImageRestController implements CommandLineRunner {
 //        decodedBytes = Base64.getDecoder().decode(encodedString);
 //        ImageModel thorlak = new ImageModel(3, "thorlak", "jpg", decodedBytes);
 
+    	//creates the first mock image
         File toUpload = new File("src/main/resources/image/eh_rehtian_climates.png");
         Map uploadResult = cloudinary.uploader().upload(toUpload, ObjectUtils.emptyMap());
         ImageModel climates = new ImageModel(1, "eh_rehtian_climates", "png", uploadResult.get("url").toString());
 
+        //creates the second mock image
         toUpload = new File("src/main/resources/image/eh_rehtian_topology.png");
         uploadResult = cloudinary.uploader().upload(toUpload, ObjectUtils.emptyMap());
         ImageModel topology = new ImageModel(2, "eh_rehtian_topology", "png", uploadResult.get("url").toString());
 
+        //creates the third mock image
         toUpload = new File("src/main/resources/image/thorlak.jpg");
         uploadResult = cloudinary.uploader().upload(toUpload, ObjectUtils.emptyMap());
         ImageModel thorlak = new ImageModel(3, "thorlak", "jpg", uploadResult.get("url").toString());
